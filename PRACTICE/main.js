28c8f12c5be15fa6ebb07705cbded98660d37281
LuchiAdaobi <Luchichronicles@gmail.com>
2022-03-22 18:07:58 +0100

    control flow 'Eloquent JS; book

---
+++ PRACTICE/main.js
@@ -0,0 +1,11 @@
+let result = 1;
+const counter = 0;
+for (let i = 0; i < 10; i += 1) {
+  result += result;
+}
+console.log(result);
+
+while (counter > 10) {
+  result += result;
+}
+console.log(result)
